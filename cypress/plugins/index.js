02bb815c5fb6e4d9b98711f04d2ff16221715574
Lalit Tewari <lalittew@gmail.com>
2023-01-30 19:21:02 +0000

    Installed HTML reporting

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,16 @@
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = (on, config) => {
+  require('cypress-mochawesome-reporter/plugin')(on);
   on("file:preprocessor", cucumber());
+  on('before:run', async(details) => {
+      console.log('override before:run');
+      await beforeRunHook(details);
+  });
+
+  on('after:run', async() => {
+      console.log('override after:run');
+      await afterRunHook();
+  });
 }
